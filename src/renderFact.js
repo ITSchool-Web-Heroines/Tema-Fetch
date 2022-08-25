ff9c6aad22451f1ec754deaff0a15fc84baf81aa
Cornel <cornel@borina.ro>
2022-08-25 20:02:08 +0300

    Tema

---
+++ src/renderFact.js
@@ -0,0 +1,3 @@
+export default function renderFact(fact) {
+    document.getElementById("cat-fact").innerText = fact;
+}
---
+++ src/script.js
@@ -0,0 +1,15 @@
+import renderFact from "./renderFact"
+
+const btn = document.querySelector("#cat-fact");
+
+btn.addEventListener("click", () => {
+    let fact = "";
+    /**
+     * Folosește API-ul https://catfact.ninja/fact
+     * pentru a afișa o informație interesantă pentru pisici
+     */
+
+
+    // Folosește această funcție, căreia îi dai un string
+    renderFact(fact);
+})
