d95044d18d5d7d9dc235a28795621f94dd72ff7b
AliceIoana <aliceioana1007@gmail.com>
2022-08-30 11:19:49 +0300

    solution

--- src/script.js
+++ src/script.js
@@ -1,15 +1,16 @@
-import renderFact from "./renderFact"
+import renderFact from "./renderFact.js";
 
-const btn = document.querySelector("#cat-fact");
+const btn = document.querySelector("#get-cat-fact");
 
 btn.addEventListener("click", () => {
-    let fact = "";
-    /**
-     * Folosește API-ul https://catfact.ninja/fact
-     * pentru a afișa o informație interesantă pentru pisici
-     */
-
-
-    // Folosește această funcție, căreia îi dai un string
-    renderFact(fact);
-})
+  let fact = "";
+  const promise = fetch("https://catfact.ninja/fact");
+  promise
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonResponse) => {
+      const rasp = renderFact(jsonResponse.fact);
+      console.log(rasp);
+    });
+});
