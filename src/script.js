b6f4712b7ed93ff32a5cc9e64c50e429f03d30c8
NicoA8 <nicoastancai@gmail.com>
2022-08-30 09:59:59 +0300

    Rezolvare

--- src/script.js
+++ src/script.js
@@ -1,15 +1,15 @@
-import renderFact from "./renderFact"
-
-const btn = document.querySelector("#cat-fact");
-
+import renderFact from "./renderFact";
+const btn = document.querySelector("#get-cat-fact");
 btn.addEventListener("click", () => {
-    let fact = "";
-    /**
-     * Folosește API-ul https://catfact.ninja/fact
-     * pentru a afișa o informație interesantă pentru pisici
-     */
-
-
-    // Folosește această funcție, căreia îi dai un string
-    renderFact(fact);
-})
+  let fact = "";
+  console.log("S-a apasat butonul.");
+  const promise = fetch("https://catfact.ninja/fact");
+  promise
+    .then(response => {
+      return response.json();
+    })
+    .then(jsonResponse => {
+      console.log("In consola:", jsonResponse.fact);
+      renderFact(jsonResponse.fact);
+    });
+});
